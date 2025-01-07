f78d7e9fa1c5d660e6f0cd4d5ba070983abd825e
Oktay Genç <gncoktay3866@gmail.com>
2025-01-08 00:30:32 +0300

    fix: Refactor UserSessionManager to return username if currentUserEmail is null

--- app/src/main/java/com/oktaygenc/cinechoice/utils/UserSessionManager.kt
+++ app/src/main/java/com/oktaygenc/cinechoice/utils/UserSessionManager.kt
@@ -1,9 +1,11 @@
 package com.oktaygenc.cinechoice.utils
 
 import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
 
 object UserSessionManager {
     private var currentUserEmail: String? = null
+    private val firebaseAuth = FirebaseAuth.getInstance()
 
     fun setCurrentUser(email: String) {
         // Email'den @ işaretinden önceki kısmı al
@@ -11,8 +13,12 @@ object UserSessionManager {
         Log.d("UserSessionManager", "Current user set to: $currentUserEmail")
     }
 
+    private fun getUserName(): String {
+        return firebaseAuth.currentUser?.email?.substringBefore("@") ?: "Unknown User"
+    }
+
     fun getCurrentUser(): String {
-        return currentUserEmail ?: "oktaygenc"
+        return currentUserEmail ?: getUserName()  // Burada getUserName() fonksiyonunu çağırıyoruz
     }
 
     fun clearCurrentUser() {
