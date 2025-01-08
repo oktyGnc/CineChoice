cf2477e0c649fbdb91fb356660886d442cd31dec
Oktay Genç <gncoktay3866@gmail.com>
2025-01-08 21:23:20 +0300

    ref: FavoriteCard ui change it

--- app/src/main/java/com/oktaygenc/cinechoice/ui/presentation/favorite/components/FavoriteCard.kt
+++ app/src/main/java/com/oktaygenc/cinechoice/ui/presentation/favorite/components/FavoriteCard.kt
@@ -1,14 +1,18 @@
 package com.oktaygenc.cinechoice.ui.presentation.favorite.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -17,7 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import com.oktaygenc.cinechoice.data.model.entitiy.Movie
@@ -31,40 +35,68 @@ fun FavoriteCard(
 ) {
     Card(
         modifier = Modifier
-            .padding(8.dp)
             .fillMaxWidth()
-
+            .padding(vertical = 8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        )
     ) {
-        Column {
-            Box {
-                AsyncImage(
-                    model = getImageUrl(movie.image),
-                    contentDescription = movie.name,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(200.dp),
-                    contentScale = ContentScale.Crop
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp) // Padding inside the card
+        ) {
+            // Image Section
+            AsyncImage(
+                model = getImageUrl(movie.image),
+                contentDescription = movie.name,
+                modifier = Modifier
+                    .size(60.dp)
+                    .padding(end = 12.dp),
+                contentScale = ContentScale.Crop
+            )
+
+            // Movie details Section
+            Column(
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.weight(1f) // To make the text stretch in the remaining space
+            ) {
+                Text(
+                    text = movie.name,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.bodyLarge
                 )
 
-                IconButton(
-                    onClick = onRemoveClick, modifier = Modifier.align(Alignment.TopEnd)
-                ) {
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Text(
+                    text = movie.category, style = MaterialTheme.typography.bodyMedium
+                )
+
+                Spacer(modifier = Modifier.height(4.dp))
+
+                Row {
                     Icon(
-                        imageVector = Icons.Default.Delete,
+                        imageVector = Icons.Default.Star,
                         contentDescription = "Remove from favorites",
+                        modifier = Modifier.size(20.dp),
                         tint = SelectedButtonColor
+                    )
+                    Text(
+                        text = movie.rating.toString(), style = MaterialTheme.typography.bodyMedium
                     )
                 }
             }
 
-            Column(
-                modifier = Modifier.padding(8.dp)
+            // Remove button Section
+            IconButton(
+                onClick = onRemoveClick, modifier = Modifier.align(Alignment.CenterVertically)
             ) {
-                Text(
-                    text = movie.name,
-                    style = MaterialTheme.typography.titleMedium,
-                    maxLines = 1,
-                    overflow = TextOverflow.Ellipsis
+                Icon(
+                    imageVector = Icons.Filled.Delete,
+                    contentDescription = "Remove from favorites",
+                    tint = SelectedButtonColor
                 )
             }
         }
